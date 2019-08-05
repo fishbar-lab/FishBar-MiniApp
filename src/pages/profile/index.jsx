2261d5dbfe41be98f9ce6d7c02d58d27cadfc5e9
shadow88sky <119136016@qq.com>
2019-08-05 10:47:34 +0800

    我的页面

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,44 +1,75 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
-import { AtButton, AtSearchBar } from 'taro-ui'
+import { AtAvatar, AtList, AtListItem } from 'taro-ui'
 import './index.styl'
+import 'taro-ui/dist/style/components/flex.scss'
+import myPost from '../../assets/images/mypost.jpg'
+import collection from '../../assets/images/collection.jpg'
+import complaints from '../../assets/images/complaints.jpg'
+import arrow from '../../assets/images/arrow.jpg'
 
 export default class Index extends Component {
 
   config = {
-    navigationBarTitleText: '我的'
+    navigationBarTitleText: '我的',
   }
 
-  constructor () {
+  constructor() {
     super(...arguments)
     this.state = {
       searchVal: ''
     }
   }
 
-  componentWillMount () {}
+  componentWillMount() { }
 
-  componentDidMount () { }
+  componentDidMount() { }
 
-  componentWillUnmount () { }
+  componentWillUnmount() { }
 
-  componentDidShow () { }
+  componentDidShow() { }
 
-  componentDidHide () { }
+  componentDidHide() { }
 
-  onChange (value) {
+  onChange(value) {
     this.setState({
       value: value
     })
   }
 
-  render () {
+  render() {
     return (
-      <View className='index'>
-        <AtSearchBar
-          value={this.state.value}
-          onChange={this.onChange.bind(this)}
-          />
+      <View className='at-col'>
+        <View className='at-row top'>
+          <View className='at-col at-col-3 top-height'>
+            <AtAvatar circle image='https://pic.qqtn.com/up/2019-4/2019040917510740548.jpg' className='avatar'></AtAvatar>
+          </View>
+          <View className='at-col at-col-6'>
+            <span id='username'>云深不知处</span>
+          </View>
+          <View className='at-col at-col-3'>
+            <button className='switch'>切换账号</button>
+          </View>
+        </View>
+        <View className='at-row center'>
+          <ul>
+            <li>
+              <img src={myPost} className='icon'></img>
+              <span className='myspan'>我的帖子</span>
+              <a href='#' className='arrow'></a>
+            </li>
+            <li>
+              <img src={collection} className='icon'></img>
+              <span className='myspan'>我的收藏</span>
+              <a href='#' className='arrow'></a>
+            </li>
+            <li>
+              <img src={complaints} className='icon'></img>
+              <span className='myspan'>投诉反馈</span>
+              <a href='#' className='arrow'></a>
+            </li>
+          </ul>
+        </View>
       </View>
     )
   }
